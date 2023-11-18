b746e7ffa1c8ec35b6b8493f0e94efcee2af720e
Billan Abdi <billanabdi04@gmail.com>
2023-11-18 17:09:09 +0400

    Solution

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function findSeven(numbers) {
+  if (numbers.includes(7)) {
+    return "Found";
+  } else return "Not found";
+}
+
+let results = findSeven([1, 2, 3, 4, 5, 6, 7, 8]);
+
+console.log(results);
